28107b7770542b020542e94eb37ea2c488572fda
Adrian7195 <143678689+Adrian7195@users.noreply.github.com>
2023-09-02 15:49:37 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 17; // Cambia el valor de num según sea necesario
+
+        if (num > 0 && num % 2 != 0) {
+            System.out.println("true");
+        }
+    }
+}
