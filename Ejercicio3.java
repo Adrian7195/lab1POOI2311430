f0b113641c0b1a2307b3babf6254b57de540353e
Adrian7195 <143678689+Adrian7195@users.noreply.github.com>
2023-09-02 15:22:03 -0500

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int numA = 10; // Cambia el valor de numA según sea necesario
+        int numB = 15; // Cambia el valor de numB según sea necesario
+
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        }
+    }
+}
